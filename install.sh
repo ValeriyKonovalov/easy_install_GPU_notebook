6f4d54363a83a0a5303a36768f714efc4c9ce2fd
Valeriy <48785349+ValeriyKonovalov@users.noreply.github.com>
2019-10-02 22:57:42 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,16 +12,35 @@ su v
 sudo grub2-mkconfig -o /boot/grub2/grub.cfg
 #### EFI:   ####
 #sudo grub2-mkconfig -o /boot/efi/EFI/centos/grub.cfg
-
-
+#
+#get and install proprietary driver Nvidia
 wget http://us.download.nvidia.com/XFree86/Linux-x86_64/430.50/NVIDIA-Linux-x86_64-430.50.run
-sudo chmod +x NVIDIA-Linux-x86_64-430.50.run
-sudo yum install epel-release dkms libstdc++.i686
+bash NVIDIA-Linux-x86_64-*
+
+# install last docker
+sudo yum -y install -y yum-utils \
+  device-mapper-persistent-data \
+  lvm2
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum -y install docker-ce docker-ce-cli containerd.io
+sudo systemctl start docker
+# install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+
+
+
+
+#sudo yum install epel-release dkms libstdc++.i686
 # get actusl version https://www.nvidia.com/en-us/drivers/unix/
 
 
-echo "Installing CUDA..."
+#echo "Installing CUDA..."
 
-wget https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-repo-rhel7-10.1.243-1.x86_64.rpm
-sudo rpm -i cuda-repo-*.rpm
-sudo yum install cuda
+#wget https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-repo-rhel7-10.1.243-1.x86_64.rpm
+#sudo rpm -i cuda-repo-*.rpm
+#sudo yum install cuda
